64315980bc636aa436c9557860d890fb34b0f55a
Javierrzrr <wafijavier41@gmail.com>
2026-09-21 13:57:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihan2/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.latihan2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "screen1") {
+        composable("screen1") {
+            Screen1(navController = navController)
+        }
+        composable("screen2") {
+            Screen2()
+        }
+    }
+}
+
+@Composable
+fun Screen1(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "NIM_KAMU - NAMA_KAMU")
+        Text(text = "Screen 1", modifier = Modifier.padding(top = 16.dp))
+        Button(
+            onClick = { navController.navigate("screen2") },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text("Ke Halaman 2")
+        }
+    }
+}
+
+@Composable
+fun Screen2() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "NIM_KAMU - NAMA_KAMU")
+        Text(text = "Screen 2", modifier = Modifier.padding(top = 16.dp))
+    }
+}
